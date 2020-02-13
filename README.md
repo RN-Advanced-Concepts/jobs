c292fa23fe46c173c3e98375c3b8a8916ca7207f
fotis tsakiris <footios76@gmail.com>
2020-02-13 15:48:05 +0200

    Navigation installed + screens added

---
+++ README.md
@@ -0,0 +1,8 @@
+Course on Udemy
+[React Native - Advanced Concepts](https://www.udemy.com/course/react-native-advanced/learn/lecture/6845216#content)
+
+
+
+
+### Packages:
+* react-navigation
---
+++ screens/AuthScreen.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AuthScreen extends Component {
+	render() {
+		return (
+			<View>
+				<View>
+					<Text>AuthScreen</Text>{' '}
+				</View>
+				<View>
+					<Text>AuthScreen</Text>{' '}
+				</View>
+				<View>
+					<Text>AuthScreen</Text>{' '}
+				</View>
+				<View>
+					<Text>AuthScreen</Text>{' '}
+				</View>
+				<View>
+					<Text>AuthScreen</Text>{' '}
+				</View>
+			</View>
+		);
+	}
+}
+
+export default AuthScreen;
